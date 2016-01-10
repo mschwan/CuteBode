cc37354daeca479d74a4f595ba5432b38b5831a8
mschwan <mjschwan@gmx.de>
2016-01-10 16:52:06 +0100

    update documentation

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['calculatemagnitude',['calculateMagnitude',['../classPlot.html#a1c6fda8fe644da0e0f0df6bf659455de',1,'Plot']]],
-  ['calculatexaxis',['calculateXAxis',['../classPlot.html#a0a85fd66f164e906708074112bd2d997',1,'Plot']]],
+  ['calculatephase',['calculatePhase',['../classPlot.html#afaf47513d7e2390b1c51df94a43a025e',1,'Plot']]],
   ['cutebode',['CuteBode',['../md_README.html',1,'']]]
 ];
